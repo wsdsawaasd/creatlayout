5cf93c5e0cc7ad85fc67f9af61cdcbaede2a379f
Phat <phatt6898@gmail.com>
2019-04-05 15:53:57 +0700

    add file

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css.css">
+</head>
+<body>
+<div class="container">
+    <header><?php require "header.php"; ?></header>
+    <nav><?php require "nav.php"; ?></nav>
+    <article> <?php require "content.php"; ?></article>
+    <footer><?php require "footer.php"; ?></footer>
+</div>
+</body>
+</html>
